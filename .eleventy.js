a8eac6b5db89e1d126caa94f877cddb4260bae74
Geoff Blair <geoff@mousepox.io>
2022-02-20 10:01:24 -0700

    🚚 Move static assets from htdocs to static folder

--- .eleventy.js
+++ .eleventy.js
@@ -2,21 +2,21 @@ const rss = require("@11ty/eleventy-plugin-rss");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(config) {
-  config.addPlugin(rss);
-  config.addPlugin(syntaxHighlight);
-  return {
-    dir: {
-      input: "htdocs",
-      output: "dist",
-    },
-    templateFormats: [
-      "md",
-      "txt",
-      "css",
-      "ico",
-      "png",
-      "jpg",
-      "njk",
-    ],
-  };
+	config.addPlugin(rss);
+	config.addPlugin(syntaxHighlight);
+
+	config.addPassthroughCopy({
+		"static": ".",
+	});
+
+	return {
+		dir: {
+			input: "htdocs",
+			output: "dist",
+		},
+		templateFormats: [
+			"md",
+			"njk",
+		],
+	};
 };
