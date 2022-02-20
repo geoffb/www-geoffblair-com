9357655b4ae9d677a0a938ec615c916a18375908
Geoff Blair <geoff@mousepox.io>
2022-02-20 17:19:04 +0000

    ⚡️ Minify HTML output

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const rss = require("@11ty/eleventy-plugin-rss");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const htmlmin = require("html-minifier");
 
 module.exports = function(config) {
 	config.addPlugin(rss);
@@ -7,6 +8,18 @@ module.exports = function(config) {
 
 	config.addPassthroughCopy({
 		"static": ".",
+	});
+
+	config.addTransform("htmlmin", function(content) {
+		if (this.outputPath && this.outputPath.endsWith(".html")) {
+			const minified = htmlmin.minify(content, {
+				useShortDoctype: true,
+				removeComments: true,
+				collapseWhitespace: true
+			});
+			return minified;
+		}
+		return content;
 	});
 
 	return {
