694c7a739313bb73b5d1e982a901565afe05baf6
Geoff Blair <geoff@mousepox.io>
2023-07-16 13:56:57 -0600

    🔧 Enable strict mode for Eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const rss = require("@11ty/eleventy-plugin-rss");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlmin = require("html-minifier");
